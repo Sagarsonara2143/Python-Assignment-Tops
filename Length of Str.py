cc7f5b5198b0cd683a8b35202de6bead36d156f0
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-03-14 21:40:55 +0530

    14.03.2023

---
+++ Length of Str.py
@@ -0,0 +1,16 @@
+# Write a Python program to calculate the length of a string.
+
+str = input("Enter String : ")
+
+print(len(str))         # using library function
+
+
+char = 0
+for i in str:           # Using For loop
+    char = char + 1
+    
+print(char)
+
+
+
+
