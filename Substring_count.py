d8bb55fead9a24006b442eb98bebd5be5b59237c
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-03-15 22:26:38 +0530

    15.03.2023

---
+++ Substring_count.py
@@ -0,0 +1,17 @@
+str = input("Enter String : ").upper()
+
+sub = input("Enter Sub string to check : ").upper()
+
+
+print(str.count(sub))
+
+
+
+count = 0
+'''
+for i in range(0,len(str)):
+    if str.find(sub):
+        count = count + 1
+
+print(count)
+'''
