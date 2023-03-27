93504c670448b08087aec700c02fa07d5955ad79
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-03-27 21:11:11 +0530

    27.03.2023

--- Common_num_in_list.py
+++ Common_num_in_list.py
@@ -1,31 +1,17 @@
 # Write a Python function that takes two lists and returns true if they have at least one common member.
 
 def common_num(list1,list2):
-    common_list = []
     for i in list1:
         for j in list2:
             if i == j:
-                common_list.append(i)
-    print("Common Number in both List : ",common_list)
-    return ""
+                return True
+    return False
 
-
-l1 = []
-l2 = []
-
-n = int(input("Enter Length of List 1 : "))
-for i in range(n):
-    data = int(input("Enter Data for List 1 : "))
-    l1.append(data)
-
-
-m = int(input("Enter Length of List 2 : "))
-for j in range(m):
-    data = int(input("Enter Data for List 1 : "))
-    l2.append(data)
+l1 = [10,15,20,25,30,35,40,45,50]
+l2 = [1,2,3,4,5,6,7,8,9,10]
 
 print("*"*50)
-print("List 1 is : ",l1)
-print("List 2 is : ",l2)
+print("List 1 : ",l1)
+print("List 2 : ",l2)
 print(common_num(l1,l2))
 print("*"*50)
