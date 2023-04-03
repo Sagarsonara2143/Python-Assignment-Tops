89c0ea6cc69731d3788bd2e4bb83f1e8263b25c9
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-03 20:37:10 +0530

    03.04.2023

--- Append_file_and_read.py
+++ Append_file_and_read.py
@@ -1,1 +1,11 @@
 #Write a Python program to append text to a file and display the text.
+
+file = open("example.txt","a")
+
+file.write("This is Appended Text\n")
+file.write("Second line of appended text\n")
+file.close()
+
+file=open("example.txt","r")
+print(file.read())
+file.close()
